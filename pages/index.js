aa66e2490ef94a0d1f98de04c3f1dd2566aa6fc2
JiaYin03 <samanthakokjiayin@gmail.com>
2023-12-08 20:30:16 +0800

    update

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import HeroSection from "g/components/HeroSection"
+
+export default function Home(){
+  return(
+    <div className="flex min-h-screen flex-col">
+      <HeroSection />
+    </div>
+  );
+}
+
